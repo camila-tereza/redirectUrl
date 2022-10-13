6767b1b683321e3318b424c36333460804d88dbe
camila.tereza <camila.tereza@assertivasolucoes.com.br>
2022-10-13 14:26:43 -0300

    Criando um link encurtador

--- src/main/java/com/onboarding/redirectUrl/controller/UrlController.java
+++ src/main/java/com/onboarding/redirectUrl/controller/UrlController.java
@@ -1,11 +1,24 @@
 package com.onboarding.redirectUrl.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.onboarding.redirectUrl.model.Url;
+import com.onboarding.redirectUrl.repository.UrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 public class UrlController {
+
+    @Autowired
+    private UrlRepository repository;
 
     @GetMapping("/gitHub")
     public RedirectView git(){
@@ -20,4 +33,14 @@ public class UrlController {
         redirect.setUrl("https://assertivasolucoes.com.br/");
         return redirect;
     }
+
+    @PostMapping ("/create")
+    @Transactional
+    public ResponseEntity<Url> createUrl(@RequestBody Url newSite, UriComponentsBuilder uriBuilder){
+        repository.save(newSite);
+
+        URI uri =uriBuilder.path("/{name}").buildAndExpand(newSite.getName()).toUri();
+        return ResponseEntity.created(uri).body(newSite);
+    }
+
 }
